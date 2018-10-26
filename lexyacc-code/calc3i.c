0219e3298a44a25c3ab2ac690a3cf455987732dc
Kuankuan Yang <yangkuankuanchn@gmail.com>
2018-10-26 21:06:00 +0200

    optimize code style

--- lexyacc-code/calc3i.c
+++ lexyacc-code/calc3i.c
@@ -4,19 +4,19 @@
 
 static int lbl;
 
-void mycmp(int flag) {	// flag: 1: <, 2: >, 3: GE, 4: LE, 5: NE, 6: EQ
+void mycmp(int flag) {  // flag: 1: <, 2: >, 3: GE, 4: LE, 5: NE, 6: EQ
   printf("\tpopq\t%%rdi\n");
   printf("\tpopq\t%%rax\n");
   printf("\tcmpq\t%%rdi, %%rax\n");
   switch(flag) {
-  	case 1: printf("\tjge"); break;
-  	case 2: printf("\tjle"); break;
-  	case 3: printf("\tjl"); break;
-  	case 4: printf("\tjg"); break;
-  	case 5: printf("\tje"); break;
-  	case 6: printf("\tjne"); break;
-  	}
-	}
+      case 1: printf("\tjge"); break;
+      case 2: printf("\tjle"); break;
+      case 3: printf("\tjl"); break;
+      case 4: printf("\tjg"); break;
+      case 5: printf("\tje"); break;
+      case 6: printf("\tjne"); break;
+  }
+}
 
 int ex(nodeType *p) {
     int lbl1, lbl2;
@@ -79,74 +79,74 @@ int ex(nodeType *p) {
             printf("\tpushq\t%%rax\n");
             // printf("\tneg\n");
             break;
-				case FACT:
-  	    		ex(p->opr.op[0]);
-	   			 	printf("\tcall fact\n");
-	   	 			break;
-				case LNTWO:
-	    			ex(p->opr.op[0]);
-	    			printf("\tlntwo\n");
-	    			break;
+        case FACT:
+            ex(p->opr.op[0]);
+            printf("\tcall fact\n");
+            break;
+        case LNTWO:
+            ex(p->opr.op[0]);
+            printf("\tlntwo\n");
+            break;
         default:
             ex(p->opr.op[0]);
             ex(p->opr.op[1]);
             switch(p->opr.oper) {
-	    					case GCD:   printf("\tgcd\n"); break;
-            		case '+':
-            			printf("\tpopq\t%%rbx\n");
-            			printf("\tpopq\t%%rax\n");
-            			printf("\taddq\t%%rbx, %%rax\n");
-            			printf("\tpushq\t%%rax\n");
-            			// printf("\tadd\n");
-            			break;
-            		case '-':
-            			printf("\tpopq\t%%rbx\n");
-            			printf("\tpopq\t%%rax\n");
-            			printf("\tsub\t%%rbx, %%rax\n");
-            			printf("\tpushq\t%%rax\n");
-            			// printf("\tsub\n");
-            			break; 
-            		case '*':
-            			printf("\tpopq\t%%rdi\n");
-            			printf("\tpopq\t%%rax\n");
-            			printf("\timulq\t%%rdi\n"); // multiply it by the contents of %rax, then places the low 64 bits of the result in %rax, high 64 bits in %rdx
-            			printf("\tpushq\t%%rax\n");
-            			break;
-            			// printf("\tmul\n"); break;
-            		case '/':											// backward version of '*'
-            			printf("\tpopq\t%%rdi\n");
-            			printf("\tpopq\t%%rax\n");
-            			// printf("\tcdqo\n");					// sign-extend %rax into %rdx // cdqo unavailable in my computer (ubuntu 64-bit)
-            			printf("\tcqo\n"); 
-                  printf("\tidivq\t%%rdi\n");	// divide %rdx:%rax by %rdi, leaving result in %rax
-            			printf("\tpushq\t%%rax\n");
-            			// printf("\tdiv\n");
-            			break;
-            		// CMP two different registers, then sets a few bits in an internal EFLAGS registers
-            		// CMP a, b ==> calculate a - b, modify the flag
-            		case '<':
-            			mycmp(1);
-            			// printf("\tpopq\t%%rdi\n");	// for example, a<100, in this case, %rdi will be the $100 (second) and %rax (first) will be the 'a'. %rax < %rdi
-            			// printf("\tpopq\t%%rax\n");
-            			// printf("\tcmpq\t%%rdi, %%rax\n");
-            			// printf("\tjge");
-            			// printf("\tcompLT\n");
-            			break;
-            		case '>':
-            			mycmp(2);
-            			break;
-            		case GE:
-            			mycmp(3);
-            			break;
-            		case LE:
-            			mycmp(4);
-            			break;
-            		case NE:
-            			mycmp(5);
-            			break;
-            		case EQ:
-            			mycmp(6);
-            			break;
+                case GCD:   printf("\tgcd\n"); break;
+                case '+':
+                            printf("\tpopq\t%%rbx\n");
+                            printf("\tpopq\t%%rax\n");
+                            printf("\taddq\t%%rbx, %%rax\n");
+                            printf("\tpushq\t%%rax\n");
+                            // printf("\tadd\n");
+                            break;
+                case '-':
+                            printf("\tpopq\t%%rbx\n");
+                            printf("\tpopq\t%%rax\n");
+                            printf("\tsub\t%%rbx, %%rax\n");
+                            printf("\tpushq\t%%rax\n");
+                            // printf("\tsub\n");
+                            break;
+                case '*':
+                            printf("\tpopq\t%%rdi\n");
+                            printf("\tpopq\t%%rax\n");
+                            printf("\timulq\t%%rdi\n"); // multiply it by the contents of %rax, then places the low 64 bits of the result in %rax, high 64 bits in %rdx
+                            printf("\tpushq\t%%rax\n");
+                            break;
+                            // printf("\tmul\n"); break;
+                case '/':   // backward version of '*'
+                            printf("\tpopq\t%%rdi\n");
+                            printf("\tpopq\t%%rax\n");
+                            // printf("\tcdqo\n");      // sign-extend %rax into %rdx // cdqo unavailable in my computer (ubuntu 64-bit)
+                            printf("\tcqo\n");
+                            printf("\tidivq\t%%rdi\n"); // divide %rdx:%rax by %rdi, leaving result in %rax
+                            printf("\tpushq\t%%rax\n");
+                            // printf("\tdiv\n");
+                            break;
+                            // CMP two different registers, then sets a few bits in an internal EFLAGS registers
+                            // // CMP a, b ==> calculate a - b, modify the flag
+                case '<':
+                            mycmp(1);
+                            // printf("\tpopq\t%%rdi\n");	// for example, a<100, in this case, %rdi will be the $100 (second) and %rax (first) will be the 'a'. %rax < %rdi
+                            // printf("\tpopq\t%%rax\n");
+                            // printf("\tcmpq\t%%rdi, %%rax\n");
+                            // printf("\tjge");
+                            // printf("\tcompLT\n");
+                            break;
+                case '>':
+                            mycmp(2);
+                            break;
+                case GE:
+                            mycmp(3);
+                            break;
+                case LE:
+                            mycmp(4);
+                            break;
+                case NE:
+                            mycmp(5);
+                            break;
+                case EQ:
+                            mycmp(6);
+                            break;
             }
         }
     }
--- src/fact.s
+++ src/fact.s
@@ -1,12 +1,13 @@
- .text
+ .section .text
  .global fact
 fact:
   movq  8(%rsp), %rdi
   movq  $1, %rax
   cmpq  $0, %rdi
-  je    fact_return        /* when fact 0, jump to end directly */
+  je    fact_return       /* when fact 0, jump to end directly */
+
 fact_loop:
-  imulq %rdi          /* %rdi = %rax * %rdi */
+  imulq %rdi              /* %rdi = %rax * %rdi */
   subq  $1, %rdi
   cmpq  $0, %rdi
   jne   fact_loop
