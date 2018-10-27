160602830e04b404f52671a8cf941ca9d4c776be
Kuankuan Yang <yangkuankuanchn@gmail.com>
2018-10-27 16:51:45 +0200

    implement gcd.s

---
+++ src/gcd.s
@@ -0,0 +1,42 @@
+/*
+int gcd(int a, int b) {
+  while (b != 0){
+    int r = b;
+    b = a % b;
+    a = r;
+  }
+  return a;
+}
+*/
+ .section .text
+ .global gcd
+gcd:
+  /* %rdi -> b */
+  /* %rsi -> a */
+  /* %rax -> result */
+
+  /* if a == 0 or b == 0 */
+  cmp   $0, %rdi
+  je    gcd_return
+  cmp   $0, %rsi
+  je    gcd_return
+
+  jmp gcd_loop
+
+gcd_loop:
+  movq  %rdi, tmp
+  movq  %rsi, %rax
+  idivq %rdi
+  movq  %rdx, %rdi
+  movq  tmp, %rsi
+
+  cmp   $0, %rdi
+  jn    gcd_loop
+
+  movq  %rsi, %rax
+  jmp   gcd_return
+
+
+gcd_return:
+  /* result already in %rax */
+  ret
