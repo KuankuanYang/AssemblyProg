5c53f8491d2fd94f4608b9bafd0e0481e560e345
Kuankuan Yang <yangkuankuanchn@gmail.com>
2018-10-27 17:02:56 +0200

    modify testforsfile.sh

--- testforsfile.sh
+++ testforsfile.sh
@@ -5,8 +5,8 @@ rm test.exe
 
 touch test.s
 cat lib/prologue.s >> test.s
-cat testprogs/lntwo.calc | ./bin/calc3i.exe >> test.s
+cat testprogs/gcd.calc | ./bin/calc3i.exe >> test.s
 cat lib/epilogue.s >> test.s
 
-gcc -no-pie test.s src/lntwo.s -o test.exe
+gcc -no-pie test.s src/gcd.s -o test.exe
 ./test.exe
